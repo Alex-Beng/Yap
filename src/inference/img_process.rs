df6c6e983052ac5ac503364c0c796d4adaef13b7
Alex Beng <599267065@qq.com>
2023-08-01 04:35:26 +0800

    add inference_5strings api

--- src/inference/img_process.rs
+++ src/inference/img_process.rs
@@ -204,8 +204,8 @@ pub fn run_match_template(
     let res_x = res_mm.min_value_location.0 as i32;
     let res_y = res_mm.min_value_location.1 as i32;
     let res_val = res_mm.min_value;
-    trace!("res_x = {}, res_y = {}, res_val = {}", res_x, res_y, res_val);
-    // info!("res_x = {}, res_y = {}, res_val = {}", res_x, res_y, res_val);
+    // trace!("res_x = {}, res_y = {}, res_val = {}", res_x, res_y, res_val);
+    info!("res_x = {}, res_y = {}, res_val = {}", res_x, res_y, res_val);
     if res_val < threshold {
         (res_x, res_y)
     } else {
--- src/inference/inference.rs
+++ src/inference/inference.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
 use std::io::Read;
 
+use log::info;
+use log::warn;
 use tract_onnx::prelude::*;
 use tract_onnx::Onnx;
 use serde_json::{Result, Value};
@@ -13,6 +15,7 @@ type ModelType = RunnableModel<TypedFact, Box<dyn TypedOp>, Graph<TypedFact, Box
 
 pub struct CRNNModel {
     model: ModelType,
+    model5: ModelType,
     index_2_word: Vec<String>,
 
     pub avg_inference_time: f64,
@@ -28,12 +31,20 @@ impl CRNNModel {
         //     .into_runnable().unwrap();
         // let mut bytes = include_bytes!("../../models/model_acc100-epoch12.onnx");
         let mut bytes = include_bytes!("../../models/model_training.onnx");
-
+        
         let model = tract_onnx::onnx()
             .model_for_read(&mut bytes.as_bytes()).unwrap()
             .with_input_fact(0, InferenceFact::dt_shape(f32::datum_type(), tvec!(1, 1, 32, 384))).unwrap()
             .into_optimized().unwrap()
             .into_runnable().unwrap();
+        
+        // 一次推理五张图
+        let model5 = tract_onnx::onnx()
+        .model_for_read(&mut bytes.as_bytes()).unwrap()
+        .with_input_fact(0, InferenceFact::dt_shape(f32::datum_type(), tvec!(5, 1, 32, 384))).unwrap()
+        .into_optimized().unwrap()
+        .into_runnable().unwrap();
+        
 
         // let content = utils::read_file_to_string(String::from("models/index_2_word.json"));
         let content = String::from(include_str!("../../models/index_2_word.json"));
@@ -52,6 +63,7 @@ impl CRNNModel {
 
         CRNNModel {
             model,
+            model5,
             index_2_word,
 
             avg_inference_time: 0.0,
@@ -90,5 +102,53 @@ impl CRNNModel {
         }
 
         ans
+    }
+    pub fn inference_5strings(&self, imgs: [&RawImage; 5]) -> [String; 5] {
+        // 似乎用不太上，跟直接用五次 1, 1, 32, 384的推理时间差不多
+        let mut strings:[String; 5] = [
+            String::from(""),
+            String::from(""),
+            String::from(""),
+            String::from(""),
+            String::from(""),
+        ];
+
+        let tensor: Tensor = tract_ndarray::Array4::from_shape_fn((5, 1, 32, 384), |(i, _, y, x)| {
+            let index = imgs[i].w * y as u32 + x as u32;
+            imgs[i].data[index as usize]
+        }).into();
+
+        let result = self.model5.run(tvec!(tensor)).unwrap();
+        let arr = result[0].to_array_view::<f32>().unwrap();
+
+        let shape = arr.shape();
+
+        // info!("arr shape: {:?}", arr.shape());
+        for i  in 0..5 {
+            
+            let mut ans = String::new();
+            let mut last_word = String::new();
+            for j in 0..shape[0] {
+                let mut max_index = 0;
+                let mut max_value = -1.0;
+                for k in 0..self.index_2_word.len() {
+                    let value = arr[[j, 0, k]];
+                    if value > max_value {
+                        max_value = value;
+                        max_index = k;
+                    }
+                }
+                let word = &self.index_2_word[max_index];
+                if *word != last_word && word != "-" {
+                    ans = ans + word;
+                }
+
+                last_word = word.clone();
+            }
+            strings[i] = ans;
+        }
+        
+        
+        strings
     }
 }
