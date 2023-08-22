1997a49bddad68ab916d532a70f8439144983d03
Alex Beng <599267065@qq.com>
2023-08-22 16:18:47 +0800

    add name-filte strategy to validate-lexicon script

--- script/valid_lexicon.py
+++ script/valid_lexicon.py
@@ -1,5 +1,7 @@
 import json
 from PIL import Image
+import cv2
+import numpy as np
 
 all_name = json.load(open('./models/all_list.json', 'r', encoding='utf-8'))
 
@@ -14,8 +16,17 @@ x = json.load(open('./xx.json', 'r', encoding='utf-8'))
 y = json.load(open('./yy.json', 'r', encoding='utf-8'))
 
 not_in_lexicon = set({})
+not_in_all_name = set({})
 for i, lb in enumerate(y):
-
+    if lb == "幸运儿银":
+        img = Image.open(x[i])
+        img = np.array(img)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        cv2.imshow('img', img)
+        cv2.waitKey(0)
+    if lb not in all_name:
+        print(lb)
+        not_in_all_name.add(lb)
     for c in lb:
         if c not in lexicon:
             print(lb)
@@ -25,3 +36,4 @@ for i, lb in enumerate(y):
             not_in_lexicon.add(c)
             break
 print(not_in_lexicon)
+print(not_in_all_name)
